686d88e1ef7640a86e0fe9af32ac729d2ac7adfa
Itzzyaboyterr0code <itzzyaboyterr@protonmail.com>
2023-01-03 16:12:19 +0100

    initial commit

---
+++ src/main/java/net/itzzyaboyterr/tnal/Tnal.java
@@ -0,0 +1,15 @@
+package net.itzzyaboyterr.tnal;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tnal implements ModInitializer {
+	public static final String MOD_ID = "tnal";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
